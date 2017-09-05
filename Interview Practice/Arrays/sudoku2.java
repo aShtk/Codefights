810c8fb96a99d4fa3651bcd7b85816fbfcaba869
aShtk <aShtk@users.noreply.github.com>
2017-09-04 22:58:15 -0500

    Update sudoku2.java

--- Interview Practice/Arrays/sudoku2.java
+++ Interview Practice/Arrays/sudoku2.java
@@ -1,1 +1,15 @@
-boolean sudoku2(char[][] grid) {    Set<Integer> rows = new HashSet<>();    Set<Integer> columns = new HashSet<>();    Set<Integer> quadrant = new HashSet<>();        for(int i = 0; i < grid.length; i++){        for(char j : i){            if(Character.isDigit(j)){                lines[j-49]++;                  if(lines[j-49] > 1) return false;            }         }    }                return true;}
+boolean sudoku2(char[][] grid) {
+    Set<Integer> rows = new HashSet<>();
+    Set<Integer> columns = new HashSet<>();
+    Set<Integer> quadrant = new HashSet<>();
+    
+    for(int i = 0; i < grid.length; i++){
+        for(char j : i){
+            if(Character.isDigit(j)){
+                lines[j-49]++;  
+                if(lines[j-49] > 1) return false;
+            } 
+        }
+    }
+    return true;
+}
