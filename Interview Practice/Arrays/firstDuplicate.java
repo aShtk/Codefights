a4fd2f69148c8244873bd7f7fe448c53148b4e47
aShtk <aShtk@users.noreply.github.com>
2017-09-04 22:57:27 -0500

    Update firstDuplicate.java

--- Interview Practice/Arrays/firstDuplicate.java
+++ Interview Practice/Arrays/firstDuplicate.java
@@ -1,7 +1,7 @@
-int firstDuplicate(std::vector<int> a) {
-    int* indexes = new int[a.size()-1];
-    for (auto &i : a)
-        if (++indexes[i-1]==2)
-        return i;
+int firstDuplicate(int[] a) {
+    for (int i:a){
+        if (a[Math.abs(i)-1]<0) return Math.abs(i);
+        a[Math.abs(i)-1]*=-1;
+    }
     return -1;
 }
